540f10bd0aad688164b837ec171b91c73a2488e6
Harsh-Prajapati02 <harsh.prajapati0206@gmail.com>
2024-09-30 12:45:22 +0530

    update-1

--- My Portfolio/src/components/Navbar.jsx
+++ My Portfolio/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets'
+import { Logo, menu, close } from '../assets'
 
 const Navbar = () => {
 
@@ -19,7 +19,7 @@ const Navbar = () => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <img src={Logo} alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold curson-pointer flex'>Harsh. &nbsp;<span className='sm:block hidden'> | &nbsp;FullStack Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
